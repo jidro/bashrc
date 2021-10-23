d2b2c0c5650e9fbcb265779d632be5e4d8e27720
jidro <z3069566588@163.com>
2021-10-23 21:08:00 +0800

    mx-bashrc

---
+++ mx-bashrc/install.sh
@@ -0,0 +1,12 @@
+#!/bin/env bash
+# Back up .bashrc to .bashrc.bak.
+mv ~/.bashrc ~/.bashrc.bak
+
+# Rename Arch-Bashrc to.bashrc.
+cp mx-bashrc .bashrc
+
+# Move the renamed.bashrc to your home directory.
+mv .bashrc ~/
+
+# Use the source command to enable the changed Bashrc.
+source ~/.bashrc
